55820c75eaeb2faa869c294cba8a71a2b13ac145
Eagle <noemail>
2026-06-07 21:42:01 +0200

    -- Added end room for Sector 2 (by Bored_Coyote). -- Added persistent save system mod (still needs a few fixes).

--- Map/World Code.js
+++ Map/World Code.js
@@ -1,33 +1,83 @@
-/*
-*/
-
 //==============================================
 // Callbacks (be careful with race conditions)
 //==============================================
 onPlayerJoin = (playerId, fromGameReset) => {
+    Save.load(playerId)
     Respawn.init(playerId)
     WelcomeGift.init(playerId)
 }
+onPlayerLeave = (playerId, serverIsShuttingDown) => Save.store(playerId)
+doPeriodicSave = () => Save.backup()
 onMobKilledPlayer = (attackingMob, killedPlayer, damageDealt, withItem) => {
-    returnValue = Respawn.keepInventory()
-    return returnValue
+    let keepInventory = Respawn.keepInventory()
+    return keepInventory
 }
-onPlayerKilledOtherPlayer = (a, b, c, d) => {
-    returnValue = Respawn.keepInventory()
-    return returnValue
+onPlayerKilledOtherPlayer = (attackingPlayer, killedPlayer, damageDealt, withItem) => {
+    let keepInventory = Respawn.keepInventory()
+    return keepInventory
 }
-onPlayerDamagingOtherPlayer = (attackingPlayer, damagedPlayer, damageDealt, withItem, bodyPartHit, damagerDbId) => {
-    Respawn.checkProtection(damagedPlayer, damageDealt)
+onPlayerDamagingOtherPlayer = (attackingPlayer, damagedPlayer, damageDealt, withItem, bodyPartHit, damagerDbId) => Respawn.checkProtection(damagedPlayer, damageDealt)
+onMobDamagingPlayer = (attackingMob, damagedPlayer, damageDealt, withItem) => Respawn.checkProtection(damagedPlayer, damageDealt)
+onPlayerMoveInvenItem = (playerId, fromIdx, toStartIdx, toEndIdx, amount) => {
+    let preventMove = Save.checkMove(playerId, fromIdx)
+    return preventMove
 }
-onMobDamagingPlayer = (attackingMob, damagedPlayer, damageDealt, withItem) => {
-    Respawn.checkProtection(damagedPlayer, damageDealt)
+onPlayerDropItem = (playerId, x, y, z, itemName, itemAmount, fromIdx) => {
+    let preventDrop = Save.checkDrop(playerId, fromIdx)
+    return preventDrop
 }
-
+//====Delete later (tests/debugging)====
+onPlayerClickUp = (playerId, b, c, d, e, f, g) => {
+    //Save.backup()
+}
 onWorldChangeBlock = (x, y, z, fromBlock, toBlock, initiatorDbId, extraInfo) => {api.log("[onWorldChangeBlock] ${toBlock}")}
 onPlayerSpawnMob = (playerId, mobId, mobType, x, y, z, mobHerdId, playSoundOnSpawn) => {api.log("[onPlayerSpawnMob] ${mobId} | ${mobType}")}
+//====Delete later (tests/debugging)====
+
 //===========================
 // Mods
 //===========================
+//Likely a dependency to many mods.
+class Save{
+    //Use inventory if false.
+    static useMoonstoneChest = true
+    static movable = false
+    //Bottom-right slot.
+    static inventorySlot = 10
+    //Top-left slot.
+    static chestSlot = 51
+    static slot = () => Save.useMoonstoneChest ? Save.chestSlot : Save.inventorySlot
+    static icon = "Lucky Block"
+    //Temporary storage indexed by playerId.
+    static activeData = {}
+    static displayName = "Save Data"
+    static defaultData = {}
+    static backup(){
+        let storeSave = Save.useMoonstoneChest ? api.setMoonstoneChestItemSlot : api.setItemSlot
+        for(playerId in Save.activeData){
+            if(!api.playerIsInGame(playerId)){continue}
+            let attributes = {customDisplayName: Save.displayName, customAttributes: Save.activeData[playerId]}
+            storeSave(playerId, Save.slot(), Save.icon, 1, attributes)
+        }
+    }
+    static store(playerId){
+        let storeSave = Save.useMoonstoneChest ? api.setMoonstoneChestItemSlot : api.setItemSlot
+        let attributes = {customDisplayName: Save.displayName, customAttributes: Save.activeData[playerId]}
+        storeSave(playerId, Save.slot(), Save.icon, 1, attributes)
+    }
+    static load(playerId){
+        let loadSave = Save.useMoonstoneChest ? api.getMoonstoneChestItemSlot : api.getItemSlot
+        Save.activeData[playerId] = loadSave(playerId, Save.slot())?.attributes.customAttributes
+    }
+    static checkMove(playerId, slot){
+        if(Save.movable || slot != Save.slot()){return}
+        return "preventChange"
+    }
+    static checkDrop(playerId, slot){
+        if(Save.movable || slot != Save.slot()){return}
+        return "preventDrop"
+    }
+}
 class Respawn{
     static auto = true
     static keepInventory = true
@@ -36,9 +86,8 @@ class Respawn{
     static protectionTime = 1000
     static lastRespawns = {}
     static init(playerId){
-        let name = api.getEntityName(playerId)
-        Respawn.lastRespawns[name] = 0;
         api.setClientOptions(playerId, {autoRespawn: Respawn.auto, secsToRespawn: Respawn.delay})
+        Respawn.lastRespawns[playerId] = 0;
     }
     static keepInventory(){
         if(Respawn.keepInventory){return "keepInventory"}
@@ -46,11 +95,10 @@ class Respawn{
     static checkProtection(damagedPlayer, damageDealt){
         let currentHealth = api.getHealth(damagedPlayer)
         if(damageDealt < currentHealth){return}
-        let name = api.getEntityName(damagedPlayer)
-        let elapsed = api.now() - Respawn.lastRespawns[name]
+        let elapsed = api.now() - Respawn.lastRespawns[damagedPlayer]
         if(elapsed < Respawn.protectionTime){return}
         api.killLifeform(damagedPlayer)
-        Respawn.lastRespawns[name] = api.now()
+        Respawn.lastRespawns[damagedPlayer] = api.now()
     }
 }
 class WelcomeGift{
@@ -58,13 +106,14 @@ class WelcomeGift{
     static items = [{type: "Jump Potion", amount: 1, attributes: {customDescription: "Welcome Gift!"}}]
     static message = ""
     static init(playerId){
-        let name = api.getEntityName(playerId)
-        if(name in WelcomeGift.giftedPlayers){
-            api.log("Already gifted")
-            return
-        }
-        if(WelcomeGift.message != null && WelcomeGift.message != ""){api.sendMessage(playerId, WelcomeGift.message)}
+        let saveEntry = "WelcomeGift.received"
+        let saveData = Save.activeData[playerId]
+        api.log(Save.activeData)
+        if(saveEntry in saveData){return}
+        if(WelcomeGift.message != ""){api.sendMessage(playerId, WelcomeGift.message)}
         WelcomeGift.items.forEach((x) => api.giveItem(playerId, x.type, x.amount, x.attributes))
-        WelcomeGift.giftedPlayers[name] = true
+        saveData[saveEntry] = true
     }
 }
+class BoatTracker{}
+class ProgressTracker{}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Notes:
 - Map currently contains two sectors (both tested to be possible) with no ending.
 - Teleport coordinates may be incorrect because of how the origin of the schematics works when importing. Will possibly require offsetting the map to its intended origin/position or fixing each sector code block.
-- Persistent data was attempted via database (from the API) and global data, to no avail. In the current state of the code, progress will likely be lost when the lobby instance closes (no client connected) and bugs (duplicate one-off items) may happen since it wasn't thoroughly tested because of these limitations. It seems the only way to have truly persistent data is to take up inventory space with special items (what the previous approaches tried to avoid). Maybe there's a way to serialize the entire database into a single inventory item with custom attributes, but that hasn't been tested at this time.
+- Persistent save system is implemented, but currently requires bug fixes to be propagated to other map mods.
